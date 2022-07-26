727206a6d075f408182a87a1ab828ab2f31a3fd3
jokeum <joke0111@gmail.com>
2022-07-26 13:17:22 +0800

    change to moda logo

--- _config.ts
+++ _config.ts
@@ -1,11 +1,13 @@
 import lume from "lume/mod.ts";
 import postcss from "lume/plugins/postcss.ts";
 import sass from "lume/plugins/sass.ts";
+import svgo from "lume/plugins/svgo.ts";
 
 const site = lume();
 
 site.use(postcss());
 site.use(sass());
+site.use(svgo());
 site.loadAssets([".js"]);
 
 export default site;
