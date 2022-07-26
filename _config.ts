e4c6a6b180b024f270047510e750b7a4288d57d2
jokeum <joke0111@gmail.com>
2022-07-26 21:59:43 +0800

    add metas and favicon

--- _config.ts
+++ _config.ts
@@ -2,12 +2,16 @@ import lume from "lume/mod.ts";
 import postcss from "lume/plugins/postcss.ts";
 import sass from "lume/plugins/sass.ts";
 import svgo from "lume/plugins/svgo.ts";
+import metas from "lume/plugins/metas.ts";
 
 const site = lume();
 
 site.use(postcss());
 site.use(sass());
 site.use(svgo());
+site.use(metas());
+site.ignore("README.md", "CHANGELOG.md");
+site.copy("img/favicon.ico", "favicon.ico");
 site.loadAssets([".js"]);
 
 export default site;
